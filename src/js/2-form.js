a7c562dc58338888c2c8d3e94f6c4e9404e10068
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2024-03-12 19:52:48 +0200

    addcomit

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,46 +4,53 @@ document.addEventListener('DOMContentLoaded', () => {
   const savedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
 
   if (savedFormData) {
-    feedbackForm.elements.email.value = savedFormData.email || '';
-    feedbackForm.elements.message.value = savedFormData.message || '';
+    feedbackForm.elements.email.value = savedFormData.email.trim() || '';
+    feedbackForm.elements.message.value = savedFormData.message.trim() || '';
   }
 
-  feedbackForm.addEventListener('input', event => {
+  feedbackForm.addEventListener('input', (event) => {
     if (event.target.name === 'email' || event.target.name === 'message') {
       const formData = {
-        email: feedbackForm.elements.email.value,
-        message: feedbackForm.elements.message.value,
+        email: feedbackForm.elements.email.value.trim(),
+        message: feedbackForm.elements.message.value.trim(),
       };
 
       localStorage.setItem('feedback-form-state', JSON.stringify(formData));
     }
   });
 
-  feedbackForm.addEventListener('submit', event => {
+  feedbackForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const formData = {
-      email: feedbackForm.elements.email.value,
-      message: feedbackForm.elements.message.value,
-    };
+    const trimmedEmail = feedbackForm.elements.email.value.trim();
+    const trimmedMessage = feedbackForm.elements.message.value.trim();
 
-    console.log('Form data submitted:', formData);
+    if (trimmedEmail && trimmedMessage) {
+      const formData = {
+        email: trimmedEmail,
+        message: trimmedMessage,
+      };
 
-    feedbackForm.reset();
-    localStorage.removeItem('feedback-form-state');
+      console.log('Form data submitted:', formData);
+
+      feedbackForm.reset();
+      localStorage.removeItem('feedback-form-state');
+    } else {
+      alert('Both email and message fields must be filled.');
+    }
   });
 });
 
 document.addEventListener('DOMContentLoaded', () => {
   const emailInput = document.querySelector('.emailInputFild');
 
-  emailInput.addEventListener('focus', event => {
+  emailInput.addEventListener('focus', (event) => {
     if (event.target.value.trim() === '') {
       event.target.setAttribute('placeholder', 'Enter your email');
     }
   });
 
-  emailInput.addEventListener('blur', event => {
+  emailInput.addEventListener('blur', (event) => {
     if (event.target.value.trim() === '') {
       event.target.removeAttribute('placeholder');
     }
